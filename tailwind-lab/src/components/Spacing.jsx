31ffad8a7fc628c9d35252a1af04463cdb479568
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-03 22:16:18 +0100

    work with spacing

---
+++ tailwind-lab/src/components/Spacing.jsx
@@ -0,0 +1,18 @@
+export default function Spacing() {
+    return (
+        <section className="bg-amber-600 rounded text-xl text-white px-8 py-6 space-y-3">
+            <h1 className="text-3xl font-semibold mb-2">Fun Facts About Animals That Will Blow Your Mind</h1>
+            <p className="mt-10 mb-10">Polar bears have black skin.</p>
+            <p>Chinese water deer swim island to island in search of food.</p>
+            <p>The bat is the only mammal that can fly.</p>
+            <p>Narwhal tusks are really an "inside out" tooth.</p>
+            <p>Gorillas can catch human colds and other illnesses.</p>
+            <p>A grizzly bear's bite is strong enough to crush a bowling ball.</p>
+            <p>Snakes only eat animals.</p>
+            <p>Pigeons can do math.</p>
+            <p>A group of ferrets is called a business.</p>
+            <p>A group of owls is called a parliament.</p>
+            <p>A group of rhinos is called a crash.</p>
+        </section>
+    );
+}
