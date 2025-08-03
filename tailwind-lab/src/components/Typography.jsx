d47554b5913f917454909033f638f449d5a936e9
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-03 22:35:45 +0100

    work with Typography

---
+++ tailwind-lab/src/components/Typography.jsx
@@ -0,0 +1,13 @@
+export default function Typography() {
+    return (
+        <div>
+            <p className="text-xl">Gorillas can catch human colds and other illnesses.</p>
+            <p className="text-red-500">A grizzly bear's bite is strong enough to crush a bowling ball.</p>
+            <p className="font-bold">Snakes only eat animals.</p>
+            <p className="text-right">Pigeons can do math.</p>
+            <p className="underline">A group of ferrets is called a business.</p>
+            <p className="tracking-widest">A group of owls is called a parliament.</p>
+            <p className="leading-40">A group of rhinos is called a crash.</p>
+        </div>
+    );
+}
