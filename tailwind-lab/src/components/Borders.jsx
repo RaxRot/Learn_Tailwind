ef334209d0bbfd607f994066e7abfef947d83fd2
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-03 22:39:27 +0100

    work with Boarders

---
+++ tailwind-lab/src/components/Borders.jsx
@@ -0,0 +1,10 @@
+export default function Borders() {
+    return (
+        <div className="space-y-4">
+            <p className="border">Gorillas can catch human colds and other illnesses.</p>
+            <p className="border border-red-600">A grizzly bear's bite is strong enough to crush a bowling ball.</p>
+            <p className="border-t-2 border-b-8 border-blue-400">Snakes only eat animals.</p>
+            <p className="border rounded-2xl">Pigeons can do math.</p>
+        </div>
+    );
+}
