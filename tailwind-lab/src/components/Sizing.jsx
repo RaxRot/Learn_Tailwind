614c13a09b86259d985b8926960ae95501f01832
Vlad Bulahov <dasistperfektosss@gmail.com>
2025-08-03 22:21:45 +0100

    work with sizing

---
+++ tailwind-lab/src/components/Sizing.jsx
@@ -0,0 +1,9 @@
+export default function Sizing() {
+    return (
+        <div>
+            <p className="w-20">Chinese water deer swim island to island in search of food.</p>
+            <p className="w-40 mt-5">The bat is the only mammal that can fly.</p>
+            <p className="w-auto mt-5">Narwhal tusks are really an "inside out" tooth.</p>
+        </div>
+    );
+}
